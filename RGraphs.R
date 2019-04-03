4fcc3cb7e197a08f6f6cbd11b8bf2eba3285c57f
Aman Neo <36672975+neoaman@users.noreply.github.com>
2019-04-04 03:44:18 +0530

    Add files via upload

--- RGraphs.R
+++ RGraphs.R
@@ -4,8 +4,9 @@ library(tidyr)  # drop na
 library(ggplot2)
 
 Cars <- drop_na(Cars93)
-data_type <- data.frame(sapply(Cars,class));
+nrow(Cars93)
 
+data_type <- data.frame(sapply(Cars,class));
 
 #1.0.0 Histogram________________________________________________________________________________________________
 #1.1.1 simple Histogram
@@ -60,7 +61,8 @@ barplot(height = data)
 
 barplot(height = table(Cars$Type))
 
-barplot(height = data,legend.text = rownames(data),col = c("lightblue","forestgreen"))
+barplot(height = data,legend.text = rownames(data),
+        col = c("lightblue","forestgreen"))
 
 barplot(height = data,col = c("lightblue","forestgreen"))
 legend(x="topr",legend = rownames(data),fill = c("lightblue","forestgreen"),title = "Car Origin",bg = "gray")
@@ -68,7 +70,7 @@ legend(x="topr",legend = rownames(data),fill = c("lightblue","forestgreen"),titl
 #2.1.2 simple bar plot with dodge position
 
 barplot(height = data,col = c("lightblue","forestgreen"),beside = T)
-legend(x="topr",legend = rownames(data),fill = c("lightblue","forestgreen"),title = "Car Origin",bg = "white",bty = "n") #remove legend box using bty="n"
+legend(x="topr",legend = rownames(data),fill = c("lightblue","forestgreen"),title = "Car Origin",bg = NULL,bty = "n") #remove legend box using bty="n"
 
 #2.2.1 ggplot bar plot
 
@@ -87,20 +89,31 @@ ggplot(data = Cars,aes(x = Cars$Type,fill=Cars$Origin))+geom_bar(colour="black",
 #3.0.0 piechart______________________________________________________________________________________________
 #3.1.1 simple pie chart
 Type <- table(Cars$Type)[table(Cars$Type)!=0]
-pie(x = Type)
 
-pie(x = Type,labels = rownames(Type),col = c("lightblue","forestgreen","yellow","orange","blue")
-    ,clockwise = T,init.angle = 0,radius = 1,border = "forestgreen") #init.angle=0 means 3 o' clock
+pie(x=Type)
+
+#3.1.2 with percentage
+frme <- as.data.frame(Type)
+pie(x = Type,labels = paste(frme[,1],round(frme[,2]*100/sum(frme[,2])),"%"))
+
+pie ( x = Type,col = c("lightblue","forestgreen","yellow","orange","blue"),
+    labels = paste(frme[,1],round(frme[,2]*100/sum(frme[,2])),"%"),
+                   clockwise = T,init.angle = 0,radius = 1,border = "forestgreen") #init.angle=0 means 3 o' clock
 legend(x = "topr",legend = rownames(Type),fill = c("lightblue","forestgreen","yellow","orange","blue"))
 
 #3.2.1 ggplot pie chart
 
-data <- data.frame(table(Cars$Type))
+data <- data.frame(table(Cars$Type)[table(Cars$Type)!=0])
 colnames(data) <- c("Type","Frequency")
 ggplot(data = data,aes(x ="",y = Frequency,fill=Type ))+geom_bar(stat = "identity")+ coord_polar("y",start=0)+
-  theme(axis.text   = element_blank())+
+  scale_fill_manual("Type",values = c("Compact"="lightblue","Large"="forestgreen",
+                                      "Midsize" = "yellow","Small" = "orange","Sporty"="blue","Van"="black"))
+#3.2.2 ggpot pie chart with values 
+ggplot(data = data,aes(x ="",y = Frequency,fill=Type ))+geom_bar(stat = "identity")+ coord_polar("y",start=0)+
+  geom_label(aes(label =  round(Frequency/ sum(Frequency) * 100)), position = position_fill(vjust =  c(40,80,160,450,370)))+
+  theme(axis.text = element_blank(),axis.title = element_blank(),panel.background = element_blank())+
   scale_fill_manual("Type",values = c("Compact"="lightblue","Large"="forestgreen","Midsize" = "yellow","Small" = "orange","Sporty"="blue","Van"="black"))
-   
+
 #4.0.0 Line______________________________________________________________________________________________
 #4.1.1 simple function plotting
 
@@ -109,7 +122,7 @@ curve(expr = exp,from = 0,to = 10,n = 30,type = "b")
 
 X <- seq(0,10,length.out = 30)
 
-plot(x=X,y = log(X),type="b")
+plot(x=X,y = log(X),type="o")
 plot(x=X,y = exp(X),type = "b")
 
 X <- seq(0,10,length.out = 30)
@@ -117,7 +130,6 @@ Y <- log(X)
 data <- data.frame(X,Y)
 ggplot(data = data,aes(x = X))+geom_line(aes(y = Y))+geom_point(aes(y = Y))
 
-ggplot()
 
 #4.2.1 multiple lines in one plot
 X <- seq(0,10,length.out = 30)
@@ -138,14 +150,11 @@ ggplot(data = data,aes(X))+geom_line(aes(y=y1),colour="green")+geom_line(aes(y=y
 
 par(mfrow=c(2,2))
 
+hist(x = Cars$Price,main = NULL)
 curve(expr = log,from = 0,to = 10,n = 30,type = "b")
 curve(expr = exp,from = 0,to = 10,n = 30,type = "b")
 plot(x = X,y = sin(X),type = "b",col="green")
 lines(x = X,y=cos(X),col="blue",type = "b")
-
-curve(expr = sin,from = 0,to = 10,n = 30,type = "b",col="green")
-lines(x = X,y=cos(X),col="blue",type = "b")
-
 
 
 #5.0.0 Scatter Plot__________________________________________________________________________________________________
@@ -155,8 +164,8 @@ lines(x = X,y=cos(X),col="blue",type = "b")
 plot(x = Cars$EngineSize,y = Cars$Price,col="blue")
 abline(lm(Cars$Price~Cars$EngineSize),col="forestgreen",lwd=2)
 
-plot(x = Cars$EngineSize,y = Cars$Price,col="blue",xlab = "Price",ylab = "Engine size")
-abline(lm(Cars$Price~Cars$EngineSize),col="forestgreen",lwd=2)
+plot(x = Cars$EngineSize,y = Cars$Price,col="blue",ylab = "Price",xlab = "Engine size")
+abline(lm(Cars$Price~Cars$EngineSize),col="red",lwd=2,lty=2)
 #5.2.0 ggplot scatter plot
 #5.2.1
 ggplot(data = Cars,aes(x = EngineSize,y = Price))+geom_point()+geom_smooth(method = lm)
@@ -173,7 +182,25 @@ ggplot(data = Cars,aes(x = EngineSize,y = Price,colour=Origin))+geom_point()+geo
 
 ggplot(data = Cars,aes(x = EngineSize,y = Price,colour=Origin))+geom_point()+geom_density_2d()
 
-#6.0.2 Multiple plots in one Place___________________________________________
+#6.0.0 Boxplot______________________________________________________________
+
+boxplot(x = Cars$Price[Cars$Origin=="USA"])
+min(x = Cars$Price[Cars$Origin=="USA"])
+min(k)
+k = Cars$Price[Cars$Origin=="USA"]
+quantile(k)[2:4]
+IQR(k)
+k[which(k>quantile(k)[4]+(1.5*IQR(k)))]
+
+quantile(k)[2]-(1.5*IQR(k))
+
+boxplot(x = Cars$Price[Cars$Origin=="non-USA"])
+boxplot(x = cbind(Cars$Price[Cars$Origin=="non-USA"],Cars$Price[Cars$Origin=="USA"]))
+
+ggplot(data = Cars,aes(x=Origin,y=Price,colour=Origin))+geom_boxplot(outlier.colour = "red",outlier.shape = 1)
+
+
+#7.0.0 Multiple plots in one Place___________________________________________
 
 scatterplot <-ggplot(data = Cars,aes(x = EngineSize,y = Price,colour=Origin))+geom_point()+geom_density_2d()+
   theme(legend.position = "none")
@@ -201,7 +228,6 @@ xdensity
 # Marginal density plot of y (right panel)
 ydensity <- ggplot(Cars, aes(x=Price)) + 
   geom_density(alpha=.5,fill="orange") + 
-  scale_fill_manual(values = c('#999999','#E69F00')) + 
   theme(legend.position = "none")
 ydensity
 
